123aa3f6b77ae6949f8e3439b4d39f32d4c46f56
P Nagaswetha <swethaprathipati10@gmail.com>
2020-07-26 00:17:21 +0530

    calculator methods implemented

---
+++ Desktop/Calculator.java
@@ -0,0 +1,30 @@
+  
+import java.util.Scanner;
+public class Calculator {
+   public static void main(String[] args) {
+      double n;
+      double n1;
+      double a;
+      char st;
+      Scanner reader = new Scanner(System.in);
+      System.out.print("Enter two numbers: ");
+      n = reader.nextDouble();
+      n1 = reader.nextDouble();
+      System.out.print("\nEnter an operator (+, -, *, /): ");
+      st = reader.next().charAt(0);
+      switch(st) {
+         case '+': a = n + n1;
+            break;
+         case '-': a = n - n1;
+            break;
+         case '*': a = n * n1;
+            break;
+         case '/': a = n / n1;
+            break;
+      default: System.out.printf("Error! Enter correct operator");
+         return;
+      }
+      System.out.print("\nThe result is given as follows:\n");
+      System.out.printf(n + " " + st + " " + n1 + " = " + a);
+   }
+}
